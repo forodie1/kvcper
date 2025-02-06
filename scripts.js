b125b3000d39dee995d2cb9871058d56a1678c43
kvcper <121885779+forodie1@users.noreply.github.com>
2025-02-06 22:13:41 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,7 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
     backgroundMusic.loop = true; // Make the song loop
 
     const quotes = [
-        "' If the most important people in this world cheat, why shouldn't I? '",
+        "' And, when you want something, all the universe conspires un helping you to achieve it '",
+        "' If you can dream it, you can do it '",
+        "' Success is the sum of small efforts – repeated day in and day out '",
+        "' If not us, who? If not now, when? '",
+        "' Every accomplishment starts with the decision to try '",
+        "' The only person with whom you have to compare ourselves, is that you in the past '",
+        "' If you want to succeed as bad as you want to breathe, then you’ll be successful '",
+        "' Life is what happens to you, While you’re busy making other plans '",
     ];
     let quoteIndex = 0;
     
@@ -57,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     discordLink.addEventListener('click', (e) => {
         e.preventDefault();
-        navigator.clipboard.writeText('gworld.').then(() => {
+        navigator.clipboard.writeText('kpvi').then(() => {
             notification.style.display = 'block';
             setTimeout(() => {
                 notification.style.opacity = '1';
@@ -81,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
             mainTextElement.firstChild.textContent = text;
             document.title = text; // Update the title with the glitched text
             glitchIndex = (glitchIndex + 1) % glitchVariants.length;
-            setTimeout(updateText, 300); // Adjust the timing for the glitch effect
+            setTimeout(updateText, 500); // Adjust the timing for the glitch effect
         }
 
         updateText();
